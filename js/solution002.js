df133001edb14defdf9b2019a1913f6314e19d91
Kyle Meyer <kyle.meyer5@icloud.com>
2022-05-10 22:00:33 -0700

    finished solution002.js and saved before commiting this time

--- js/solution002.js
+++ js/solution002.js
@@ -2,7 +2,15 @@
 
 function repeatStr(n, s){
     console.log(s .repeat(n))
+    // worked fine in console with just the log, but was returning "expected undefined to deeply equal..." error in codewars tests, so found solution on stack overflow that needed to add a return. Added a duplicate return and passed code wars test
     return s .repeat(n)
 }
 
 repeatStr(3,'Beep')
+
+
+// Favorite solutions from examples given after solving?:
+
+    // As arrow function and without console log:
+        // repeatStr = (n, s) => s.repeat(n)
+    
