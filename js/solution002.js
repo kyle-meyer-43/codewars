6d3e1e3f94609045644b1c2d9af511492e18bed3
Kyle Meyer <kyle.meyer5@icloud.com>
2022-05-12 18:50:24 -0700

    did format document to clean up

--- js/solution002.js
+++ js/solution002.js
@@ -1,16 +1,15 @@
 // Write a function called repeatStr which repeats the given string exactly n times.
 
-function repeatStr(n, s){
-    console.log(s .repeat(n))
+function repeatStr(n, s) {
+    console.log(s.repeat(n))
     // worked fine in console with just the log, but was returning "expected undefined to deeply equal..." error in codewars tests, so found solution on stack overflow that needed to add a return. Added a duplicate return and passed code wars test
-    return s .repeat(n)
+    return s.repeat(n)
 }
 
-repeatStr(3,'Beep')
+repeatStr(3, 'Beep')
 
 
 // Favorite solutions from examples given after solving?:
 
     // As arrow function and without console log:
         // repeatStr = (n, s) => s.repeat(n)
-    
