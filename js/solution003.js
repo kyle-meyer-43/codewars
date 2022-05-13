1bb3465b269303e7af04e194fbbc6b4abdfde453
Kyle Meyer <kyle.meyer5@icloud.com>
2022-05-12 20:58:59 -0700

    PREP for solution003.js

---
+++ js/solution003.js
@@ -0,0 +1,22 @@
+// Your task is to find the first element of an array that is not consecutive.
+
+// By not consecutive we mean not exactly 1 larger than the previous element of the array.
+
+// E.g. If we have an array [1,2,3,4,6,7,8] then 1 then 2 then 3 then 4 are all consecutive but 6 is not, so that's the first non-consecutive number.
+
+// If the whole array is consecutive then return null2.
+
+// The array will always have at least 2 elements1 and all elements will be numbers. The numbers will also all be unique and in ascending order. The numbers could be positive or negative and the first non-consecutive could be either too!
+
+// P.R.E.P : Parameters, Returns, Examples, Pseudo Code:
+
+// Parameters: first non consecutive in array (not exactly one larger than previous in array). null2 return if all in array are consecutive. always at least 2 elements in the array, always numbers. will all be unique and in ascending order. can be postive or negative
+
+// Returns: Return either the first element that is not consecutive, or null2 if all are consecutive. 
+    // not defined how to return? maybe start with console log?
+
+// Examples:  If we have an array [1,2,3,4,6,7,8] then 1 then 2 then 3 then 4 are all consecutive but 6 is not, so that's the first non-consecutive number.
+
+// Pseudo code: 
+    // funtion with conditional statement that takes in array. thinking an if and else statement, don't need an else if?
+
