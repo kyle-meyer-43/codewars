8f447a65a0f8f803db2d668bfb96209bbb394951
Kyle Meyer <kyle.meyer5@icloud.com>
2022-05-16 21:31:57 -0700

    started and finished solution004.js

---
+++ js/solution004.js
@@ -0,0 +1,17 @@
+// Code as fast as you can! You need to double the integer and return it.
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i *2 ;
+  }
+
+
+// other options I liked in solutions section:
+
+    //   const doubleInteger = i => i * 2;
+
+    // function doubleInteger(i) {
+    //     // i will be an integer. Double it and return it.
+    //     return i+i;
+
+
