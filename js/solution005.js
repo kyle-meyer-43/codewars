c075866cd4e3b62830abbb4786a8af9d47b38b6c
Kyle Meyer <kyle.meyer5@icloud.com>
2022-05-17 21:01:28 -0700

    PREP for solution005

---
+++ js/solution005.js
@@ -0,0 +1,14 @@
+// 8 kyu
+// Let's play rock paper scissors!
+
+// Parameters: show the winner of the game. If a draw, return "draw!"
+
+// Return: looks like just need to return? Might be good to do console.log
+
+// examples: 
+    // rps('scissors','paper') // Player 1 won!
+    // rps('scissors','rock') // Player 2 won!
+    // rps('paper','paper') // Draw!
+
+// Pseudo code: conditional statments? would work to display which "card" (play) won, but not which player made the play
+
