070b9e9863723fa94a4204540fd2bf8fd16b0669
Kyle Meyer <kyle.meyer5@icloud.com>
2022-05-22 12:22:34 -0700

    prep for solution006

---
+++ js/solution006.js
@@ -0,0 +1,9 @@
+// 8kyu: Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// Parameters: Take in a value in method/function. Value will always be a boolean. 
+
+// Returns: if true, return "yes", if no, return "no"
+
+// Examples: true = "yes"  false + "no"
+
+// psuedo code: create a conditional function that takes in boolean and returns corresponding value
