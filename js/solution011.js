7eb3366896b053f25a817d981e5352ea186ed03f
Kyle Meyer <kylemeyer5@icloud.com>
2022-06-05 18:20:41 -0700

    solved 011

--- js/solution011.js
+++ js/solution011.js
@@ -1,3 +1,4 @@
+
 // Switch It Up!
 
 //When provided with a number between 0-9, return it in words.
@@ -10,3 +11,37 @@
 
 // Pseudo code: Function to take in a number. Conditional or switch case. Sep cases for each number 1-9, then one catch all for everyting else.
 
+let number
+function switchItUp (number){
+    switch(number){
+    case 1:
+        return('One')
+        break;
+    case 2:
+        return('Two')
+        break;
+    case 3:
+        return('Three')
+        break;
+    case 4:
+        return('Four')
+        break;
+    case 5:
+        return('Five')
+        break;
+    case 6:
+        return ('Six')
+        break;
+    case 7:
+        return ('Seven')
+        break;
+    case 8:
+        return('Eight')
+        break;
+    case 9:
+        return('Nine')
+        break;
+    default: 
+        return(text = 'Zero')
+    }
+}
