9db1b17bac5e8155fd88ca3f0ffedb2aa588dbe4
Kyle Meyer <kylemeyer5@icloud.com>
2022-06-05 18:04:13 -0700

    prep time

---
+++ js/solution011.js
@@ -0,0 +1,12 @@
+// Switch It Up!
+
+//When provided with a number between 0-9, return it in words.
+
+//Parameters: Taking in a number. Will assume it's postive and an integer. 
+
+// Returns: If number is 1-9, returning in words. All others returning just like it's input
+
+//Example: 1 comes in, 'one' goes out
+
+// Pseudo code: Function to take in a number. Conditional or switch case. Sep cases for each number 1-9, then one catch all for everyting else.
+
