02b93581113452c556c17052a5954f76c0196592
Kyle Meyer <kylemeyer5@icloud.com>
2022-06-09 18:43:24 -0700

    prep for solution012

---
+++ js/solution012.js
@@ -0,0 +1,13 @@
+//7 kyu : Validate PIN code for ATM
+
+// Parameters: Taking in attempt at making new PIN code. Good PINS are strings with either exactly 4 or 6 digits. All others that have different amount of digits or non numbers(like letters) are invalid/not compatible
+
+//returns: If it is a valid PIN string return true. If not return false
+
+// examples: 
+    //3567  -- > true
+    //56731 --> false
+    //h4g7  --> false
+
+//Pseudo code: take in attempt, evaluate, return result of true or false of fulfilling requirements.
+
