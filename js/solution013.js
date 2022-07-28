a77221dc5382de09e83cef99cf1b91e6395e73c2
Kyle Meyer <kylemeyer5@icloud.com>
2022-07-28 16:26:40 -0700

    pseudo progress

---
+++ js/solution013.js
@@ -0,0 +1,26 @@
+// ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
+
+// Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
+
+
+//PARAMETERS
+// We're going to have a string. We sometimes will get numbers or specials. We don't want to change those. We'll always get english. Should also consider what to di if we get empty spaces
+
+//RETURNS
+// We want to get a new string that takes in the original string and then pushes out a string with every letter +13 from the letter that was imputted in that spot
+
+//EXAMPLES
+// 'a' would be switched 'n'
+
+//PSUEDO CODE
+//  Write a function that takes in a string
+function rot13(message){
+// create a place for the output to rest   ** Always a good idea to create a place fo the new string to rest
+let newString = ''
+// Change the letters in the inputted string to be 13 plus
+let alpha = 'abcdefghijklmnopqrstuvwxyzabcdefghijklm'
+
+
+
+
+}
