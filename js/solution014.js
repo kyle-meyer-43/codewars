23cb7030387091fdb4fc0ea27608ed5c3a7fc66c
Kyle Meyer <kylemeyer5@icloud.com>
2022-08-25 17:07:57 -0700

    coded it

--- js/solution014.js
+++ js/solution014.js
@@ -9,28 +9,49 @@
 // "#######"       ==>  ""
 // ""              ==>  "" 
 
-//P: it gives us a string. will we ever be given an empty sting? yes. Will i ever be giben spcialal characters? yes probabley. but it doesn't really matter if it's in a string
+//P: it gives us a string. will we ever be given an empty sting? yes. Will i ever be given special characters? yes probably. but it doesn't really matter if it's in a string
 
 //R: return a string that has letters that didn't have a # after them
 //E:
 
 //P: Make a function that takes in a string
-const cleanString = s => {
+// const cleanString = s => {
     
-    let ar = [];
-    for (let e of s){
+//     let ar = [];
+//     for (let e of s){
 
-        if(e !='#'){
-            ar.push(e)}
-            else if (e == '#'){
-                ar.pop() } console.log(ar)
-}
-        return ar.join('')
+//         if(e !='#'){
+//             ar.push(e)}
+//             else if (e == '#'){
+//                 ar.pop() } console.log(ar)
+// }
+//         return ar.join('')
     
-    }
+//     }
             
 
-console.log(cleanString)
+// console.log(cleanString)
+
+
+function cleanString(s) {
+    const arr = s.split("")
+    
+    const newArr = []
+    
+    arr.forEach((val, index, origArr) => {
+      if (val === "#") {
+        // don't want to call pop on an empty array
+        if (newArr.length > 0) {
+          const removed = newArr.pop();
+          console.log("removed", val)
+        }
+      } else {
+        newArr.push(val)
+      }
+    })
+    
+    return newArr.join("")
+  }
 
 //Grab the string and split it into individual characters ('a')   
 //Now it's an array. We want to map through
